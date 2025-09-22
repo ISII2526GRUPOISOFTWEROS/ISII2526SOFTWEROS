86a2e941ae80d4e564f614e68ec9adf2e0c23594
LidiaLpzPlz13 <lidia.lopez13@alu.uclm.es>
2025-09-22 13:14:33 +0200

    task1 starting

---
+++ src/AppForSEII2526.API/Models/Plan.cs
@@ -0,0 +1,15 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    [Index(nameof(Name), IsUnique = true)]
+    public class Plan
+    {
+        
+        public int Id { get; set; }
+        public int Weeks { get; set; }
+        public String HealthIssues { get; set; }
+        public String Description { get; set; }
+        public String Name { get; set; }
+        public Decimal Totalprice { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
