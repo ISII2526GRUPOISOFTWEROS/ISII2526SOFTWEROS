e227d915745998724b9d9233f46c3d962e49af19
Adrian615 <adria@ADRIAN>
2025-09-22 13:13:41 +0200

    Task 3 starting

---
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -0,0 +1,16 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Purchase
+    {
+        //Primary key 
+        public int Id { get; set; } 
+
+        //Strings
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+        public string Street { get; set; }
+        //Decimal
+        public decimal Total_prices { get; set; }
+    }
+}
