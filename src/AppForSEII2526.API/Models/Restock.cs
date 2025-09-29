c82cdea0ec4dba582deb6e92a60d7240df4fdb37
MARCOS ALEJANDRO LABRADOR DUARTE <MarcosAlejandro.Labrador@alu.uclm.es>
2025-09-29 12:15:11 +0200

    Primera asociacion de clase use case RestockItems

---
+++ src/AppForSEII2526.API/Models/Restock.cs
@@ -0,0 +1,17 @@
+﻿namespace AppForSEII2526.API.Models
+{
+
+    [Index(nameof(Title), IsUnique = true)]
+    public class Restock
+    {
+        public string DeliveryAddress { get; set; }
+        public string Description { get; set; }
+        public DateTime ExpectedDate { get; set; }
+        public int Id { get; set; }
+        public DateTime RestockDate { get; set; }
+        public string Title { get; set; }
+        public Decimal TotalPrice { get; set; }
+
+
+    }
+}
