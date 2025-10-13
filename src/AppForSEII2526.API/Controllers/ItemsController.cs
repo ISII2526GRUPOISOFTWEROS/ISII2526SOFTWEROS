ab2e1d1a14a445a5ac9e9291462cb19c2208f9f1
MARCOS ALEJANDRO LABRADOR DUARTE <MarcosAlejandro.Labrador@alu.uclm.es>
2025-10-13 12:51:34 +0200

    Creating Controller for Items

---
+++ src/AppForSEII2526.API/Controllers/ItemsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+
+        private ApplicationDbContext _context;
+        private ILogger<ItemsController> _logger;
+
+        public ItemsController(ApplicationDbContext context, ILogger<ItemsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        //[HttpGet]
+        //[Route("[action]")]
+        //[ProducesResponseType(typeof(decimal), (int)HttpStatusCode.OK)]
+        //[ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+
+        //public async Task <ActionResult> ComputeDivision(decimal op1, decimal op2)
+        //{
+        //    if(op2 == 0)
+        //    {
+        //        string error ="Can't divide by zero";
+        //        _logger.LogError(DateTime.Now+error);
+        //        return BadRequest(error);
+        //    }
+                
+        //    decimal result = op1 / op2;
+        //    return Ok(result);
+        //}
+
+
+    }
+}
