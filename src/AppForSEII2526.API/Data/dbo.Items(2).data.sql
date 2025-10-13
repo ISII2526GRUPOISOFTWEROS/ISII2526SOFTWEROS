5478d546a10b36373a017f64facef9a9f3f1f35f
Adrian615 <adria@ADRIAN>
2025-10-13 13:08:35 +0200

    Database + migration

---
+++ src/AppForSEII2526.API/Data/dbo.Items(2).data.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT [dbo].[ItemTypes] ON
+INSERT INTO [dbo].[ItemTypes] ([Id], [Name], [ClassId]) VALUES (1, N'Mancuernas', NULL)
+INSERT INTO [dbo].[ItemTypes] ([Id], [Name], [ClassId]) VALUES (2, N'Gomas', NULL)
+INSERT INTO [dbo].[ItemTypes] ([Id], [Name], [ClassId]) VALUES (3, N'Maquinas', NULL)
+SET IDENTITY_INSERT [dbo].[ItemTypes] OFF
