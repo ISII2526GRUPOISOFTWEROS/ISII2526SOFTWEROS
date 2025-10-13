7bf82e7d75ddc39812ab5fde3873899cd3e30541
LidiaLpzPlz13 <lidia.lopez13@alu.uclm.es>
2025-10-13 13:08:19 +0200

    cambio diagram

--- src/AppForSEII2526.API/Models/Brand.cs
+++ src/AppForSEII2526.API/Models/Brand.cs
@@ -1,12 +1,3 @@
 ﻿namespace AppForSEII2526.API.Models
 {
-    [Index(nameof(Name), IsUnique = true)]
-    public class Brand
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-
-        //Reference 
-        public IList<Item> Items { get; set; }
-    }
 }
