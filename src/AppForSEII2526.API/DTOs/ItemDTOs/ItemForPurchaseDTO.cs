6775b7c8f577ef103968f3e0e483a1c0fb1e099f
Adrian615 <adria@ADRIAN>
2025-10-23 09:27:59 +0200

    Fixing itemforpurchasedto

--- src/AppForSEII2526.API/DTOs/ItemDTOs/ItemForPurchaseDTO.cs
+++ src/AppForSEII2526.API/DTOs/ItemDTOs/ItemForPurchaseDTO.cs
@@ -14,7 +14,6 @@
         }
 
         public int Id { get; set; }
-
         public string? Name { get; set; } 
         public string? Brand { get; set; } 
         public string? Description { get; set; } 
