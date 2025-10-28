cc35c3d0842b14ec2c41292c915709ad768f5077
LidiaLpzPlz13 <lidia.lopez13@alu.uclm.es>
2025-10-28 09:16:12 +0100

    comit new dto

---
+++ src/AppForSEII2526.API/DTOs/ClassesDTOs/PlanForCreateDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace AppForSEII2526.API.DTOs.ClassesDTOs
+{
+    public class PlanForCreateDTO
+    {
+        public PlanForCreateDTO(string name, string? description, int weeks, string? healthIssues, string? goals, PaymentMethod paymentMethod, IList<ClassForPlanDTO> SelectedClasses)
+        {
+            name = name ?? throw new ArgumentNullException(nameof(name));
+            description = description;
+            weeks = weeks;
+            healthIssues = healthIssues;
+            goals = goals;
+            paymentMethod = paymentMethod;
+            SelectedClasses = SelectedClasses ?? throw new ArgumentNullException(nameof(SelectedClasses));
+        }
+        public PlanForCreateDTO()
+        {
+            SelectedClasses = new List<ClassForPlanDTO>();
+        }
+
+    }
+}
