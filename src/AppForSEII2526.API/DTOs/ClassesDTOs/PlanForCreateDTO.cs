e2e1bbd920b5f76b93d7c252302d3c76f8352d42
LidiaLpzPlz13 <lidia.lopez13@alu.uclm.es>
2025-10-29 09:57:26 +0100

    Comit PlanForCreate

--- src/AppForSEII2526.API/DTOs/ClassesDTOs/PlanForCreateDTO.cs
+++ src/AppForSEII2526.API/DTOs/ClassesDTOs/PlanForCreateDTO.cs
@@ -16,6 +16,59 @@
         {
             SelectedClasses = new List<ClassForPlanDTO>();
         }
+        [DataType(System.ComponentModel.DataAnnotations.DataType.MultilineText)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please, provide a name for your planning")]
+            [StringLength(50, MinimumLength = 3, ErrorMessage = "Planning name must have at least 3 characters")]
+            [Display(Name = "Planning Name")]
+            public string PlanningName { get; set; }
 
-    }
-}
+            [StringLength(200, ErrorMessage = "Description cannot exceed 200 characters")]
+            public string? Description { get; set; }
+
+            [Required(ErrorMessage = "Please, specify how many weeks you plan to attend")]
+            [Range(1, 52, ErrorMessage = "Number of weeks must be between 1 and 52")]
+            [Display(Name = "Number of Weeks")]
+            public int NumberOfWeeks { get; set; }
+
+            [Display(Name = "Health Issues (optional)")]
+            public string? HealthIssues { get; set; }
+
+            [Display(Name = "Goals (optional)")]
+            public string? Goals { get; set; }
+
+            [Required(ErrorMessage = "Please, select at least one payment method")]
+            [Display(Name = "Payment Method")]
+            public PaymentMethod PaymentMethod { get; set; }
+
+            [Required(ErrorMessage = "Please, select at least one class to plan")]
+            public IList<ClassForPlanDTO> SelectedClasses { get; set; }
+
+            [Display(Name = "Total Price")]
+            [JsonPropertyName("TotalPrice")]
+            public double TotalPrice
+        {
+                get
+            {
+                    return SelectedClasses.Sum(c => c.PricePerClass * NumberOfWeeks);
+                }
+            }
+        }
+
+        public class ClassForPlanDTO
+        {
+            [Required]
+            public string ClassName { get; set; }
+
+            [Required]
+            public string ClassType { get; set; }
+
+            [Required]
+            public double PricePerClass { get; set; }
+
+            [Required]
+            public string Day { get; set; }
+
+            [Required]
+            public string Time { get; set; }
+        }
+    } 
