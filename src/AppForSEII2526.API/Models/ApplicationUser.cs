8daf7cbb835f747b98dc9f39f889f5710c08b938
LidiaLpzPlz13 <lidia.lopez13@alu.uclm.es>
2025-10-31 17:26:54 +0100

    comit errors

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -6,6 +6,9 @@ namespace AppForSEII2526.API.Models;
 // Add profile data for application users by adding properties to the ApplicationUser class
 
 public class ApplicationUser : IdentityUser {
+    public ApplicationUser()
+    {
+    }
     public ApplicationUser(string name, string surname)
     {
     }
