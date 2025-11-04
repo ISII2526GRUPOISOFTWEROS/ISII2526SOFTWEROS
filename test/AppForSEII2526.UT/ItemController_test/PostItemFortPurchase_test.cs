b65e280f472ebc52151ae1f78fc7a536cb4dc970
Adrian615 <adria@ADRIAN>
2025-11-04 16:04:24 +0100

    Adding the class for the post test

---
+++ test/AppForSEII2526.UT/ItemController_test/PostItemFortPurchase_test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppForSEII2526.API.DTOs.ItemDTOs;
+using AppForSEII2526.API.Controllers;
+
+namespace AppForSEII2526.UT.ItemController_test
+{
+    public class PostItemFortPurchase_test : AppForSEII25264SqliteUT
+    {
+
+        public PostItemFortPurchase_test()
+        {
+
+
+        }
+    }
+}
