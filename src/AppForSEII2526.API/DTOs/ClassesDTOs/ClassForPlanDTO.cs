d942ad69e30b1ba7d684971f194ce97a28c0d699
LidiaLpzPlz13 <lidia.lopez13@alu.uclm.es>
2025-11-05 17:36:03 +0100

    ClassController

--- src/AppForSEII2526.API/DTOs/ClassesDTOs/ClassForPlanDTO.cs
+++ src/AppForSEII2526.API/DTOs/ClassesDTOs/ClassForPlanDTO.cs
@@ -1,29 +1,34 @@
 ﻿
+using Microsoft.EntityFrameworkCore;
+
 namespace AppForSEII2526.API.DTOs.ClassesDTOs
 {
     public class ClassForPlanDTO
     {
-        public ClassForPlanDTO(int id, decimal price, DateTime? date, string name, IList<string?> itemType)
+        public ClassForPlanDTO(int id, decimal price, DateTime? date, string name, int capacity, IList<string?> itemType)
         {
             Id = id;
-            price = price;
-            itemType = itemType;
-            date = date;
+            this.price = price;
+            this.itemType = itemType ?? new List<string?>();
+            this.date = date ?? default;
             Name = name;
+            this.capacity = capacity;
         }
         public int Id { get; set; }
         [Precision(10, 2)]
         public decimal price { get; set; }
         public IList<string?> itemType { get; set; }
         public DateTime date { get; set; }
-       
+
         public string Name { get; set; }
+        public int capacity { get; set; }
 
         public override bool Equals(object? obj)
         {
             return obj is ClassForPlanDTO dTO &&
                    Id == dTO.Id &&
                    price == dTO.price &&
+                   capacity == dTO.capacity &&
                    EqualityComparer<IList<string?>>.Default.Equals(itemType, dTO.itemType) &&
                    date == dTO.date &&
                    Name == dTO.Name;
