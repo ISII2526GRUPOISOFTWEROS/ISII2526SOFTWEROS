59cc0b2715eca6db73a4557f9a62eb97a2db31a7
Adrian615 <adria@ADRIAN>
2025-11-06 19:07:57 +0100

    Adding the folder and the test for the details

---
+++ test/AppForSEII2526.UT/PurchaseController_test/GetPurchaseDetails_test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppForSEII2526.API.DTOs.ItemDTOs;
+using AppForSEII2526.API.Controllers;
+namespace AppForSEII2526.UT.PurchaseController_test
+{
+    public class GetPurchaseDetails_test : AppForSEII25264SqliteUT
+    {
+    }
+}
