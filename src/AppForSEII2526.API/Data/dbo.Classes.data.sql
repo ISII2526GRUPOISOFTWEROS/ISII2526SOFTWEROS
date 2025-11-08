f47593b5e4a35105824e4c494fac3bd36353a3fc
LidiaLpzPlz13 <lidia.lopez13@alu.uclm.es>
2025-11-08 15:54:16 +0100

    classbbdd

--- src/AppForSEII2526.API/Data/dbo.Classes.data.sql
+++ src/AppForSEII2526.API/Data/dbo.Classes.data.sql
@@ -1,5 +1,5 @@
 ﻿SET IDENTITY_INSERT [dbo].[Classes] ON
-INSERT INTO [dbo].[Classes] ([Id], [Capacity], [Name], [Price], [Date]) VALUES (1, 10, N'Crossfit', CAST(30.00 AS Decimal(10, 2)), N'2025-10-20 10:00:00')
-INSERT INTO [dbo].[Classes] ([Id], [Capacity], [Name], [Price], [Date]) VALUES (2, 15, N'spinning', CAST(25.00 AS Decimal(10, 2)), N'2025-11-10 11:00:00')
-INSERT INTO [dbo].[Classes] ([Id], [Capacity], [Name], [Price], [Date]) VALUES (3, 8, N'firness', CAST(18.00 AS Decimal(10, 2)), N'2025-11-01 08:00:00')
+INSERT INTO [dbo].[Classes] ([Id], [Capacity], [Name], [Price], [Date]) VALUES (1, 15, N'Cardio', CAST(20.00 AS Decimal(10, 2)), N'2025-11-20 13:00:00')
+INSERT INTO [dbo].[Classes] ([Id], [Capacity], [Name], [Price], [Date]) VALUES (2, 20, N'Fitness', CAST(25.00 AS Decimal(10, 2)), N'2025-11-22 10:00:00')
+INSERT INTO [dbo].[Classes] ([Id], [Capacity], [Name], [Price], [Date]) VALUES (3, 25, N'CrossFit', CAST(30.00 AS Decimal(10, 2)), N'2025-11-30 08:00:00')
 SET IDENTITY_INSERT [dbo].[Classes] OFF
