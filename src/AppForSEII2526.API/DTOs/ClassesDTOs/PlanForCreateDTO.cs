801e2e8b02c550cafb78fb4742d4f187a4b36693
LidiaLpzPlz13 <lidia.lopez13@alu.uclm.es>
2025-11-10 17:16:02 +0100

    commit ut

--- src/AppForSEII2526.API/DTOs/ClassesDTOs/PlanForCreateDTO.cs
+++ src/AppForSEII2526.API/DTOs/ClassesDTOs/PlanForCreateDTO.cs
@@ -2,50 +2,42 @@
 
 namespace AppForSEII2526.API.DTOs.ClassesDTOs
 {
+
+
     public class PlanForCreateDTO
     {
+        public PlanForCreateDTO(string Name, string? description, int weeks, string? healthIssues, IList<ClassForPlanDTO> selectedClasses, PaymentMethod paymentMethod)
+        {
+            Name = Name;
+            Description = description;
+            Weeks = weeks;
+            HealthIssues = healthIssues;
+            SelectedClasses = selectedClasses;
+            PaymentMethod = paymentMethod;
+        }
         [Required]
         [StringLength(60, MinimumLength = 3, ErrorMessage = "Plan name must have at least 3 characters")]
-        public string Name { get; set; } = string.Empty;
-
+        public string Name { get; set; }
         [StringLength(100, ErrorMessage = "Description cannot exceed 100 characters")]
         public string? Description { get; set; }
-
         [Required]
         [Range(1, 52, ErrorMessage = "Weeks must be between 1 and 52")]
         public int Weeks { get; set; }
-
         [StringLength(100, ErrorMessage = "Health issues cannot exceed 100 characters")]
         public string? HealthIssues { get; set; }
-
         [Required]
-        public IList<ClassSelectionDTO> SelectedClasses { get; set; } = new List<ClassSelectionDTO>();
-
+        public IList<ClassForPlanDTO> SelectedClasses { get; set; }
         [Required]
-        public int PaymentMethodId { get; set; }  
-        public IList<GoalForClassDTO>? Goals { get; set; }
-    }
-
-    public class ClassSelectionDTO
-    {
-        [Required]
-        public int Id { get; set; }
-
-        [Required]
-        public string Name { get; set; } = string.Empty;
-
-        [Required]
-        public decimal Price { get; set; }
-
-        [Required]
-        public DateTime Date { get; set; }
-
-        public IList<string>? ItemType { get; set; }
-    }
-    public class GoalForClassDTO{
-        [Required]
-        public int ClassId { get; set; }
-
-        public string? Goal { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public override bool Equals(object? obj)
+        {
+            return obj is PlanForCreateDTO dto &&
+                   Name == dto.Name &&
+                   Description == dto.Description &&
+                   Weeks == dto.Weeks &&
+                   HealthIssues == dto.HealthIssues &&
+                   SelectedClasses.SequenceEqual(dto.SelectedClasses) &&
+                   PaymentMethod == dto.PaymentMethod;
+        }
     }
 }
