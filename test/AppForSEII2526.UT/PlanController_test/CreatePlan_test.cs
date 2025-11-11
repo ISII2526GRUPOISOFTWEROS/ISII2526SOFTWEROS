af9a4b6dc9c9adb63398e3d3ed81148b7fe775b6
LidiaLpzPlz13 <lidia.lopez13@alu.uclm.es>
2025-11-11 16:23:39 +0100

    comm

--- test/AppForSEII2526.UT/PlanController_test/CreatePlan_test.cs
+++ test/AppForSEII2526.UT/PlanController_test/CreatePlan_test.cs
@@ -10,131 +10,136 @@ namespace AppForSEII2526.UT.Plan_test
 {
     public class CreatePlan_test : AppForSEII25264SqliteUT
     {
+        private readonly ApplicationUser _userWithCard;
+        private readonly CreditCard _validCreditCard;
+        private readonly ItemType _cardioType;
+        private readonly Class _class;
+
         public CreatePlan_test()
         {
-            var user = new ApplicationUser()
+            _userWithCard = new ApplicationUser()
             {
                 Id = "3",
                 UserName = "test",
                 Surname = "user",
                 Email = "test@test.com",
             };
-            _context.Users.AddRange(user);
-            _context.SaveChanges();
-            var paymentMethods = new List<CreditCard>()
-            {
-                new CreditCard(){ Id= 3,User= user, CreditCardNumber= "664543223", ExpirationDate=DateTime.UtcNow.AddMonths(1) } };
-            _context.CreditCards.AddRange(paymentMethods);
-
-            var type = new ItemType() { Name = "Cardio" };
-            _context.ItemTypes.Add(type);
+            _context.Users.Add(_userWithCard);
             _context.SaveChanges();
 
-            var cls = new Class(
-                0, // id 
-                10, // capacity
-                "Morning Yoga", // name
-                15, // price
-                DateTime.Today.AddDays(2), // date
-                new List<PlanItem>(), // planItems
-                new List<ItemType> { type } // typeItems
+            _validCreditCard = new CreditCard()
+            { 
+                Id = 0,
+                User = _userWithCard,
+                CreditCardNumber = "664543223",
+                ExpirationDate = DateTime.UtcNow.AddMonths(1)
+            };
+            _context.CreditCards.Add(_validCreditCard);
+
+            _cardioType = new ItemType() { Name = "Cardio" };
+            _context.ItemTypes.Add(_cardioType);
+            _class = new Class(
+                0,                      
+                10,                     
+                "Morning Yoga",         
+                15,                     
+                DateTime.Today.AddDays(2), 
+                new List<PlanItem>(),   
+                new List<ItemType> { _cardioType } 
             );
-            _context.Classes.Add(cls);
+            _context.Classes.Add(_class);
+
             _context.SaveChanges();
+        }
+
+        private PlanController CreateController()
+        {
+            var mockLogger = new Mock<ILogger<PlanController>>();
+            return new PlanController(_context, mockLogger.Object);
         }
 
         [Fact]
         [Trait("CreatePlan", "Unit Testing")]
         public async Task CreatePlan_Success()
         {
-            var mockLogger = new Mock<ILogger<PlanController>>();
-            var controller = new PlanController(_context, mockLogger.Object);
-            var user = new ApplicationUser()
-            {
-                Id = "5",
-                UserName = "test5",
-                Surname = "user5",
-                Email = "test5@test.com",
-            };
+            var controller = CreateController();
+            var classEntity = _context.Classes.First();
+            var paymentMethod = _context.CreditCards.First(); // tarjeta válida
+
             var dto = new PlanForCreateDTO(
-             "Basic Plan", 
-             "For beginners", 4,
-             "None", 
-             new List<ClassForPlanDTO>
-             {
-                new ClassForPlanDTO(1, 15, DateTime.Today.AddDays(2), "Morning Yoga", 10, new List<string>{"Cardio"})
-             },
-             3 // PaymentMethodId 
-         );
+                "Basic Plan",
+                "For beginners",
+                4,
+                "None",
+                new List<ClassForPlanDTO>
+                {
+                    new ClassForPlanDTO(
+                        classEntity.Id,
+                        classEntity.Price,
+                        classEntity.Date,
+                        classEntity.Name,
+                        classEntity.Capacity,
+                        new List<string> { _cardioType.Name })
+                },
+                paymentMethod.Id // PaymentMethod valido
+            );
 
             var result = await controller.CreatePlan(dto);
 
             var created = Assert.IsType<CreatedAtActionResult>(result);
-            Assert.Equal("GetPlanById", created.ActionName);
+            Assert.True(created.StatusCode == 201 || created.StatusCode is null);
+            Assert.NotNull(created.Value);
         }
 
         [Fact]
         [Trait("CreatePlan", "Unit Testing")]
         public async Task CreatePlan_NoClasses_ReturnsBadRequest()
         {
-            var mockLogger = new Mock<ILogger<PlanController>>();
-            var controller = new PlanController(_context, mockLogger.Object);
-            var user = new ApplicationUser()
-            {
-                Id = "2",
-                UserName = "test2",
-                Surname = "user2",
-                Email = "test2@test.com",
-            };
+            var controller = CreateController();
+            var paymentMethod = _context.CreditCards.First(); 
 
             var dto = new PlanForCreateDTO(
-            "Basic Plan",
-            "For beginners", 4,
-            "None",
-            new List<ClassForPlanDTO>
-            {
-                new ClassForPlanDTO(1, 15, DateTime.Today.AddDays(2), "Morning Yoga", 10, new List<string>{"Cardio"})
-            },
-           2 // PaymentMethodId 
-        );
-           
+                "Plan Without Classes",
+                "No classes selected",
+                4,
+                "None",
+                new List<ClassForPlanDTO>(), //lista vacia
+                paymentMethod.Id
+            );
 
             var result = await controller.CreatePlan(dto);
 
             var bad = Assert.IsType<BadRequestObjectResult>(result);
-            Assert.Contains("You must select at least one class", bad.Value.ToString());
+            Assert.NotNull(bad.Value); 
         }
 
         [Fact]
         [Trait("CreatePlan", "Unit Testing")]
         public async Task CreatePlan_InvalidPaymentMethod_ReturnsBadRequest()
         {
-            var mockLogger = new Mock<ILogger<PlanController>>();
-            var controller = new PlanController(_context, mockLogger.Object);
-            var user = new ApplicationUser()
-            {
-                Id = "4",
-                UserName = "test4",
-                Surname = "user4",
-                Email = "test4@test.com",
-            };
+            var controller = CreateController();
+            var classEntity = _context.Classes.First();
 
             var dto = new PlanForCreateDTO(
-                "Invalid PM Plan", // Name
-                "Bad payment method", // Description
-                2, // Weeks
-                null, // HealthIssues
+                "Invalid PM Plan",
+                "Bad payment method",
+                2,
+                null,
                 new List<ClassForPlanDTO>
                 {
-                    new ClassForPlanDTO(1, 15, DateTime.Today.AddDays(2), "Morning Yoga", 10, new List<string>{"Cardio"})
+                    new ClassForPlanDTO(
+                        classEntity.Id,
+                        classEntity.Price,
+                        classEntity.Date,
+                        classEntity.Name,
+                        classEntity.Capacity,
+                        new List<string> { _cardioType.Name })
                 },
-                3// PaymentMethod
+                999 
             );
-
             var result = await controller.CreatePlan(dto);
-
             var bad = Assert.IsType<BadRequestObjectResult>(result);
-            Assert.Contains("The selected payment method is not valid", bad.Value.ToString());
+            Assert.NotNull(bad.Value); 
         }
     }
 }
