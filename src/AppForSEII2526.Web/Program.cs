b1e34d705989744f53a206c8adb2b38e0d4d6e02
Adrian615 <adria@ADRIAN>
2025-11-20 15:29:35 +0100

    Solving errors

--- src/AppForSEII2526.Web/Program.cs
+++ src/AppForSEII2526.Web/Program.cs
@@ -1,10 +1,11 @@
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+using AppForSEII2526.Web;
 using AppForSEII2526.Web.Components;
 using AppForSEII2526.Web.Components.Account;
 using AppForSEII2526.Web.Data;
-using AppForSEII2526.Web.API;
+using AppForSEII2526.Web.KairosAPI;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
