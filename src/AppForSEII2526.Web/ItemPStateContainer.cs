a0cec666362c5dcfe52814627ee80d20eebbd830
Adrian615 <adria@ADRIAN>
2025-11-24 11:59:14 +0100

    Adding the purchase

---
+++ src/AppForSEII2526.Web/ItemPStateContainer.cs
@@ -0,0 +1,24 @@
+﻿using AppForSEII2526.Web.API;
+using System;
+namespace AppForSEII2526.Web
+{
+    public class ItemPStateContainer
+    {
+        public ItemForPurchaseDTO? SelectedItem { get; private set; }
+
+        public event Action? OnChange;
+
+        public void SetSelectedItem(ItemForPurchaseDTO item)
+        {
+            SelectedItem = item;
+            OnChange?.Invoke();
+        }
+
+        public void ClearSelectedItem()
+        {
+            SelectedItem = null;
+            OnChange?.Invoke();
+        }
+
+    }
+}
