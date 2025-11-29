579a69951d184ca028acc37a5547e1888708a37d
Adrian615 <adria@ADRIAN>
2025-11-29 12:23:02 +0100

    Adding the constructors

--- src/AppForSEII2526.API/DTOs/PurchaseDTOs/CreatePurchaseItemDTO.cs
+++ src/AppForSEII2526.API/DTOs/PurchaseDTOs/CreatePurchaseItemDTO.cs
@@ -3,11 +3,18 @@ namespace AppForSEII2526.API.DTOs.PurchaseDTOs
 {
            public class CreatePurchaseItemDTO
         {
+             public CreatePurchaseItemDTO() { }
             public CreatePurchaseItemDTO(int itemId, int quantity)
             {
                 ItemId = itemId;
                 Quantity = quantity;
             }
+        public CreatePurchaseItemDTO(int itemId, int quantity,decimal price)
+        {
+            ItemId = itemId;
+            Quantity = quantity;
+            Price = price;
+        }
             [Required(ErrorMessage = "ItemId is required")]
             [Range(1, int.MaxValue, ErrorMessage = "ItemId must be a positive number.")]
             public int ItemId { get; set; }
@@ -20,12 +27,13 @@ namespace AppForSEII2526.API.DTOs.PurchaseDTOs
         {
             return obj is CreatePurchaseItemDTO dTO &&
                    ItemId == dTO.ItemId &&
-                   Quantity == dTO.Quantity;
+                   Quantity == dTO.Quantity &&
+                   Price == dTO.Price;
         }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(ItemId, Quantity);
+            return HashCode.Combine(ItemId, Quantity,Price);
         }
     }
     }
