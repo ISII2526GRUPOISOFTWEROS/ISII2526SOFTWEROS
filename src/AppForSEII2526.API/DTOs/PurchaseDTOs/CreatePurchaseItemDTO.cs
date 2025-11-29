1b61ab7bd229492c1f26b5fb252abd929da7f094
Adrian615 <adria@ADRIAN>
2025-11-29 12:16:45 +0100

    Adding the price

--- src/AppForSEII2526.API/DTOs/PurchaseDTOs/CreatePurchaseItemDTO.cs
+++ src/AppForSEII2526.API/DTOs/PurchaseDTOs/CreatePurchaseItemDTO.cs
@@ -15,7 +15,7 @@ namespace AppForSEII2526.API.DTOs.PurchaseDTOs
             [Required(ErrorMessage = "Quantity is required")]
             [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than zero.")]
             public int Quantity { get; set; }
-
+            public decimal Price { get; set; }
         public override bool Equals(object? obj)
         {
             return obj is CreatePurchaseItemDTO dTO &&
