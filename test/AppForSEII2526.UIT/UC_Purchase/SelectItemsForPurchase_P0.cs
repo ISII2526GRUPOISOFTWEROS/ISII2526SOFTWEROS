0a4e38c1043abcdc91e4422f26988e9cf7215c0f
Adrian615 <adria@ADRIAN>
2025-12-03 10:41:21 +0100

    Creating the PageObjetct to interact with SelectItemsForPurchase

---
+++ test/AppForSEII2526.UIT/UC_Purchase/SelectItemsForPurchase_P0.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Purchase
+{
+    public class SelectItemsForPurchase_P0 : PageObject
+    {
+        protected SelectItemsForPurchase_P0(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+    }
+}
