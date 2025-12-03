bb304a9f4b8709c74f10e92ead55c99e0a82b465
Adrian615 <adria@ADRIAN>
2025-12-03 10:57:10 +0100

    Adding the seach item by name

--- test/AppForSEII2526.UIT/UC_Purchase/SelectItemsForPurchase_P0.cs
+++ test/AppForSEII2526.UIT/UC_Purchase/SelectItemsForPurchase_P0.cs
@@ -8,8 +8,17 @@ namespace AppForSEII2526.UIT.UC_Purchase
 {
     public class SelectItemsForPurchase_P0 : PageObject
     {
+        private By inputName = By.Id("inputName");
+        private By inputBrand = By.Id("inputBrand");
+        private By buttonSearchItems = By.Id("searchItems");
         protected SelectItemsForPurchase_P0(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
         {
+        }
+        public void SearchItems(string name)
+        { // wait for the web element to be clickable
+            WaitForBeingClickable(inputName);
+            _driver.FindElement(inputName).SendKeys(name);
+            _driver.FindElement(buttonSearchItems).Click();
         }
     }
 }
