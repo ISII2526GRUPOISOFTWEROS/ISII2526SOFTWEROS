2f33d30b23bd179734ba432c2709ae027d4fec64
LidiaLpzPlz13 <lidia.lopez13@alu.uclm.es>
2025-12-03 10:57:34 +0100

    adding P0

--- test/AppForSEII2526.UIT/UC_Plan/SelectClassesForPlan_P0.cs
+++ test/AppForSEII2526.UIT/UC_Plan/SelectClassesForPlan_P0.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,14 @@ namespace AppForSEII2526.UIT.UC_Plan
 {
     public class SelectClassesForPlan_P0 : PageObject
     {
-        protected SelectClassesForPlan_P0(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
-        {
+        By Name = By.Id("className");
+        public SelectClassesForPlan_P0(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        { }
+            public void SearchClasses(string name) {
+                //wait for the webelement to be clickable
+                WaitForBeingClickable(Name);
+                _driver.FindElement(Name).SendKeys(name);
+                    }
         }
     }
-}
+
