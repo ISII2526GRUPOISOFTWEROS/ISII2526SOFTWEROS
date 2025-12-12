0707206cb40c480682bdfa2e82cfd3159406c5ea
Adrian615 <Adrian.Sevilla@alu.uclm.es>
2025-12-12 17:50:20 +0100

    Task133 develop uit for create purchase (#141)
    * Fixing the razors
    
    * Fixing the detail
    
    * Adding the name of the item in the shopping cart
    
    * Adding the BF test
    
    * Adding the scenarios
    
    * Adding the Scen 5
    
    * Fixing the scenarios
    
    * Adding the scenearios
    
    * Finishing the test for all the scenarios
    
    * Solving conflicts errors
    
    * Changing the API to azure

--- test/AppForSEII2526.UIT/UC_Purchase/SelectItemsForPurchase_P0.cs
+++ test/AppForSEII2526.UIT/UC_Purchase/SelectItemsForPurchase_P0.cs
@@ -1,35 +1,64 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
 
-//namespace AppForSEII2526.UIT.UC_Purchase
-//{
-//    public class SelectItemsForPurchase_P0 : PageObject
-//    {
-//         By inputName = By.Id("inputName");
-//         By inputBrand = By.Id("inputBrand");
-//         By buttonSearchItems = By.Id("searchItems");
-//        By tableOfItemsBy = By.Id("TableOfItems");
-//        public SelectItemsForPurchase_P0(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
-//        {
-//        }
-//        public void SearchItems(string name, string brand)
-//        { // wait for the web element to be clickable
-//            WaitForBeingClickable(inputName);
-//            _driver.FindElement(inputName).SendKeys(name);
+namespace AppForSEII2526.UIT.UC_Purchase
+{
+    public class SelectItemsForPurchase_P0 : PageObject
+    {
+         By inputName = By.Id("inputName");
+         By inputBrand = By.Id("inputBrand");
+         By buttonSearchItems = By.Id("searchItems");
+         By buttonPurchaseItems = By.Id("purchaseItemButton");
+        By tableOfItemsBy = By.Id("TableOfItems");
+
+        public SelectItemsForPurchase_P0(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+
+        public void SearchItems(string name, string brand)
+        { // wait for the web element to be clickable
+            WaitForBeingClickable(inputName);
+           _driver.FindElement(inputName).SendKeys(name);
 
 
-//            WaitForBeingClickable(inputBrand);
-//            _driver.FindElement(inputBrand).SendKeys(brand);
+            WaitForBeingClickable(inputBrand);
+           _driver.FindElement(inputBrand).SendKeys(brand);
 
-//            _driver.FindElement(buttonSearchItems).Click();
-//        }
+            WaitForBeingClickable(buttonSearchItems);
+            _driver.FindElement(buttonSearchItems).Click();
 
-//        public bool CheckListOfItems(List<string[]> expectedItems)
-//        {
-//            return CheckBodyTable(expectedItems, tableOfItemsBy);
-//        }
-//    }
-//}
+            System.Threading.Thread.Sleep(500);
+        }
+
+        public bool CheckListOfItems(List<string[]> expectedItems)
+        {
+            return CheckBodyTable(expectedItems, tableOfItemsBy);
+        }
+        public void ClickPurchaseButton()
+        {
+            WaitForBeingClickable(buttonPurchaseItems);
+            _driver.FindElement(buttonPurchaseItems).Click();
+        }
+        public void AddQuantityToItem(string itemName, int quantity)
+        {
+            By addItemButton = By.Id($"itemforpurchase_{itemName}");
+            WaitForBeingClickable(addItemButton);
+
+            for (int i = 0; i < quantity; i++) { 
+                _driver.FindElement(addItemButton).Click();
+                System.Threading.Thread.Sleep(200);
+            }
+
+        }
+        public void RemoveItemInCart(int itemId)
+        {
+            By removeItemButton = By.Id($"removeItem_{itemId}");
+            WaitForBeingClickable(removeItemButton);
+            _driver.FindElement(removeItemButton).Click();
+        }
+    }
+}
